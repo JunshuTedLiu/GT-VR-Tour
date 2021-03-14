4e3d6107974e585dd765df877c31a4f322f6a12e
Junshu Liu <junshutedliu@gmail.com>
2021-03-14 19:58:41 -0400

    Improve accuracy for real-time location
    Improved the geographical coordinate & A-Frame mapping; added sky pictures for use; cleaning up code

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 var lat;
 var lon;
 
-var latToAFrame;
-var lonToAFrame;
+var latToAFrame = 2.35;
+var lonToAFrame = -4.95;
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -26,8 +26,13 @@ function showPosition(position) {
     //Convert coordinate to a-frame position.
     //Equation:
     //1 geographical length (coord) = -5005.32491491 A-Frame length (for our model, rescaled & repositioned)
-    latToAFrame = (lat-33.775200)/(33.774000-33.775200)*(2.35-(-5.75))+(-5.75);
-    lonToAFrame = -((lon-(-84.397805))/(-84.396866-(-84.397805))*(0.25-4.95)+4.95);
+//    latToAFrame = (lat-33.775200)/(33.774000-33.775200)*(2.35-(-5.75))+(-5.75);
+//    lonToAFrame = -((lon-(-84.397805))/(-84.396866-(-84.397805))*(0.25-4.95)+4.95);
+    
+    //z
+    latToAFrame = (lat-33.775933)/(33.774124-33.775933)*(2.35-(-11.65))+(-11.65);
+    //x
+    lonToAFrame = (lon-(-84.394961))/(-84.397759-(-84.394961))*((-4.95)-12.3)+12.3;
 }
 
 function setCamera() {
@@ -35,5 +40,12 @@ function setCamera() {
     
 //     document.querySelector("#cameraWrapper").object3D.position.set(0, -1, 0);
     
-    document.getElementById('cameraWrapper').setAttribute('position', {x:lonToAFrame, y:0, z:latToAFrame});
+//    document.getElementById('cameraWrapper').setAttribute('position', {x:lonToAFrame, y:0, z:latToAFrame});
+    
+//    ONLY WORKS ONCE
+    document.querySelector('#cameraWrapper').setAttribute('position', {x:lonToAFrame, y:-2, z:latToAFrame});
+    
+//    document.querySelector('#cameraWrapper').setAttribute('position', {x:0, y:0, z:0});
+    
+    console.log("Camera updated.");
 }
